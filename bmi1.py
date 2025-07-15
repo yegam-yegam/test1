4de7d54314734238429aa6076b6b0744e3106e52
yegam <kchan080716@gmail.com>
2025-07-15 22:54:25 +0900

    Update bmi1.py

--- bmi1.py
+++ bmi1.py
@@ -1,24 +1,22 @@
 import streamlit as st
 
-# 제목
-st.title("Calculator")
+st.title("BMI 계산기")
+l = st.number_input("키(cm)", value=0)
+w = st.number_input("몸무게(kg)", value=0)
 
-# 사용자 입력
-number1 = st.number_input("Enter first number", value=0)
-operation = st.selectbox("Choose an operation", ["+", "-", "*", "/"])
-number2 = st.number_input("Enter second number", value=0)
-
-# 계산 및 결과 출력
-if st.button("Calculate"):
+if st.button("계산하기"):
     try:
-        if operation == "+":
-            result = number1 + number2
-        elif operation == "-":
-            result = number1 - number2
-        elif operation == "*":
-            result = number1 * number2
-        elif operation == "/":
-            result = number1 / number2
-        st.write(f"Result: {result}")
+        r = round(w/((l/100)**2), 2)
+        st.write(f"BMI: {r}")
+        if r <= 18:
+            st.write("저체중 입니다.")
+        if r > 18 and r <= 23:
+            st.write("정상 입니다.")       
+        if r > 23 and r <= 25:
+            st.write("과체중 입니다.")       
+        if r > 25 and r <= 30:
+            st.write("비만 입니다.")
+        if r > 30:
+            st.write("고도비만 입니다.")
     except Exception as e:
-        st.error("An error occurred!")
+        st.error("오류 발생")
