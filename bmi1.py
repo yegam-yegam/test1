336d90507c1688d4c9441ce01346bef246913aff
yegam <kchan080716@gmail.com>
2025-07-17 09:33:47 +0900

    Update bmi1.py

--- bmi1.py
+++ bmi1.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("BMI 계산기")
 l = st.number_input("키(cm)", value=0)
 w = st.number_input("몸무게(kg)", value=0)
-
+'하하하하'
 if st.button("계산하기"):
     try:
         r = round(w/((l/100)**2), 2)
